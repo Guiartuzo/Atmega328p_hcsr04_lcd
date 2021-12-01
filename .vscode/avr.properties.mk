e1ffcb7656e91b2d6877f3e9c93908c1370fb142
Guilherme <guilherme.artuzo@hotmail.com>
2021-12-01 16:24:52 -0300

    remove files not needed

--- .vscode/avr.properties.mk
+++
@@ -1,4 +0,0 @@
-AVR.source.compiler = C:\Program Files\avr8-gnu-toolchain\avr8-gnu-toolchain-win32_x86\bin\avr-gcc.exe
-AVR.source.libraries = 
-AVR.device.type = atmega328p
-AVR.device.frequency = 
